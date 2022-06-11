94f2e7f982917daa3801237474968bef6c4d0151
lalitsawantt <llaalliitt251@gmail.com>
2022-06-11 21:03:33 +0530

    playing with asyn await and promises

--- Practice/script.js
+++ Practice/script.js
@@ -1,6 +1,45 @@
-console.log("Hellllllooo");
-const sayName = document.createElement('h2');
-const Name = document.createTextNode("Lalit");
-sayName.appendChild(Name);
-const h2 = document.getElementsByTagName('h2');
-h2.appendChild(sayName);
+let p = new Promise((resolve, reject) => {
+  let num = 1 + 2;
+  // make num equals 2 to resolve, num != 2 to reject
+  if(num == 2){
+    return resolve("Promise Resolved");
+  }else{
+    return reject("Promise Rejected");
+  }
+})
+
+let consumePromise = () => {
+  p.then((res) => console.log("Resolved message : ", res))
+  .catch((rej) => console.log("Rejected message : ", rej))
+}
+
+consumePromise();
+
+let postTwoSeconds = () => {
+  let promise = new Promise((resolve, reject) => {
+  // make num equals 2 to resolve, num != 2 to reject
+
+    let num = 1 + 2;
+    if(num == 2){
+      setTimeout(() => {
+        resolve("RESOLVED")
+      },2000)
+    }else{
+      setTimeout(() => {
+        reject("REJECTED")
+      }, (2000));
+    }
+  })
+  return promise;
+}
+
+
+const getRes = async () => {
+  try{
+  const response = await postTwoSeconds();
+  console.log("RESPONSE IN GETRES : ", response);
+  }catch(error){  
+    console.log("ERROR IN CATCH : ", error)
+  }
+}
+getRes();
